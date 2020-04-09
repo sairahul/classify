a7304fceb797bdeafe5da24927231667b8872ed9
sai <sairahulreddy@yahoo.com>
2020-04-09 01:06:28 -0400

    Separated multi tasking and single tasking code for reference

--- models/utils.py
+++ models/utils.py
@@ -11,6 +11,47 @@ class CNNPretrainedModel(nn.Module):
     Customized from fastai learner
     """
 
+    def __init__(self, base_arch, no_classes, dropout=0.5, init=nn.init.kaiming_normal_):
+        super(CNNPretrainedModel, self).__init__()
+
+        self.model = create_cnn_model(base_arch, no_classes, ps=dropout)
+        self.meta = cnn_config(base_arch)
+        self.split(self.meta['split'])
+        self.freeze()
+
+        apply_init(self.model[1], init)
+
+    def split(self, split_on:SplitFuncOrIdxList)->None:
+        "Split the model at `split_on`."
+        if isinstance(split_on,Callable): split_on = split_on(self.model)
+        self.layer_groups = split_model(self.model, split_on)
+        return self
+
+    def freeze_to(self, n:int)->None:
+        "Freeze layers up to layer group `n`."
+        for g in self.layer_groups[:n]:
+            for l in g:
+                if not isinstance(l, bn_types): requires_grad(l, False)
+        for g in self.layer_groups[n:]: requires_grad(g, True)
+
+    def freeze(self)->None:
+        "Freeze up to last layer group."
+        assert(len(self.layer_groups)>1)
+        self.freeze_to(-1)
+
+    def unfreeze(self):
+        "Unfreeze entire model."
+        self.freeze_to(0)
+
+    def forward(self, x):
+        return self.model.forward(x)
+
+
+class CNNMultiTaskModel(nn.Module):
+    """
+    Customized from fastai learner
+    """
+
     def __init__(self, base_arch, no_diseases, dropout=0.5, init=nn.init.kaiming_normal_):
         super(CNNPretrainedModel, self).__init__()
 
@@ -21,11 +62,8 @@ class CNNPretrainedModel(nn.Module):
         #self.age_head = create_head(nf, 1, ps=0.5, concat_pool=True, bn_final=False)
         self.gender_head = create_head(nf, 2, ps=0.5, concat_pool=True, bn_final=False)
         #self.projection_head = create_head(nf, 3, ps=0.5, concat_pool=True, bn_final=False)
-        
-        #self.model = create_cnn_model(base_arch, no_classes, ps=dropout)
-        self.disease_model = nn.Sequential(self.body, self.disease_head)
 
-        #self.model = nn.Sequential(self.body, self.disease_head)
+        self.disease_model = nn.Sequential(self.body, self.disease_head)
 
         self.meta = cnn_config(base_arch)
         self.split(self.meta['split'])
